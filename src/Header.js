74c153fab73b08511c5b4af1404bda6e517e5a16
jokessenich <johnny.kessenich@gmail.com>
2020-05-30 11:19:16 -0700

    side by side view and mobile repsonsive

--- src/Header.js
+++ src/Header.js
@@ -8,16 +8,35 @@ import Media from './components/Media'
 import Contact from './components/Contact'
 import Blog from './components/Blog'
 import Photos from './components/Photos'
+import {slide as Menu} from 'react-burger-menu'
+
 
 export default class Main extends React.Component{
 
+    constructor(props){
+        super(props)
+        this.state ={
+            isOpen:false
+        }
+    }
+
+    handleStateChange (state){
+        this.setState({isOpen:state.isOpen})
+    }
+
+    closeMenu=()=>{
+        this.setState({
+            isOpen:false
+        })
+    }
 
 render(){
+    let menuProp = this.state.isOpen
+    
     return(
-        <div><h1>Prodigious Son</h1>
-        <h2>A Memoir of Miracles</h2>
+        <div>
         <div className="header">
-        <ul>
+        <ul className = "fullscreen-links">
             <li ><Link className="navLinks" to = '/'>Home</Link></li>
             <li><Link className="navLinks" to = '/About-Author'>Author</Link></li>
             <li><Link  className="navLinks" to = '/Purchase'>Purchase</Link></li>
@@ -28,6 +47,17 @@ render(){
             {//<li><Link className="navLinks" to = '/Photos'>Photos</Link></li>
 }
         </ul>
+        <Menu isOpen = {menuProp}
+                       width = {'100%'}
+                       right
+                      onStateChange={(state) => this.handleStateChange(state)}>
+                                      <li ><Link className="navLinks" to = '/' onClick = {this.closeMenu}>Home</Link></li>
+            <li><Link className="navLinks" to = '/About-Author' onClick = {this.closeMenu}>Author</Link></li>
+            <li><Link  className="navLinks" to = '/Purchase' onClick = {this.closeMenu}>Purchase</Link></li>
+            <li><Link  className="navLinks" to ='/Media' onClick = {this.closeMenu}>Media</Link></li>
+            <li><Link className="navLinks"  to = '/Blog' onClick = {this.closeMenu}>Blog</Link></li>
+            <li><Link className="navLinks" to = '/Contact' onClick = {this.closeMenu}>Contact</Link></li>
+        </Menu>
         </div></div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,32 +6,25 @@ export default class Home extends React.Component{
 render(){
     return(
         <div className = "page">
+                    <h1 className = "title" >Prodigious Son: A Memoir of Miracles</h1>
+        <h2>Ralph Matson</h2>
+        <h3 className="transcribed">Transcribed by: John Kessenich</h3>
                   {/*  <h1> Prodigious Son </h1>
         <h2>A Memoir of Miracles</h2>
             <div className = 'corky-image'></div>
     */}
-<img src = 'https://github.com/jokessenich/corky/blob/master/Prodigious%20Son%20BOOK%203D.jpg?raw=true' height='450' width = '300' />
-        
-
-        <div className="infoBox">
-            <h3>"CORKY" is an honest and insightful look into a life well-lived and filled with colorful tales of a time though recent yet forgotten but to those of us who lived through it .
-</h3>
-<h4>-Jack Bornoff</h4>
-<br />
-<h3>There is a tangible love felt in these pages, for every character we encounter. Every single one. Even in his depictions of heartbreak or cruelty, Matson generously renders a world—and a philosophy—of hope, kindness, and perseverance. Never have I had the pleasure of reading a memoir so thoughtful in its understanding of humanity, the preservation of memory, and a feeling of optimism that compels you to leave its pages rejuvenated and optimistic about what there is to come.</h3>
-<h4>-Sarah Tierney</h4>
-
-        </div>
-        <div className="corky-image"><div className="overlay"><p className="overlayText">"My awareness was at a pinpoint. There was no thinking, no experiencing; there was no nothing. There was me in the light as part of the light and no thought."</p></div></div>
-
-        <div className = "about-book">
-        <p>How many times can you cheat death on your path to
+    <div className ="picDescriptionBox">
+        <div className="bookCoverDiv">
+<img className = "bookImage" src = 'https://github.com/jokessenich/corky/blob/master/Prodigious%20Son%20BOOK%203D.jpg?raw=true' height='300' width = '200' />
+</div>
+<div className = "about-book">
+        <p><span className="emphasize-text">How many times can you cheat death </span>on your path to
                 enlightenment? Meet Ralph "Corky" Matson, a modern-day
                 urban shaman who consistently beats the odds. In this
                 captivating memoir, Matson leads us through Woodstock,
                 down drug alleys, over mountain ashrams and around
                 gamblers on a never-ending quest for higher consciousness
-                as he struggles with his inner demons—and angels. Born to
+                as he struggles with his inner demons—and angels. {<br />}{<br />}Born to
                 a devoted mother who struggled to protect him from the
                 unjustified hatred of a tyrannical father, Matson grew up in
                 poverty with a negative self-image that haunted him even as
@@ -52,6 +45,25 @@ render(){
                 story. Edgy, yet inspirational, Prodigious Son: A Memoir of
                 Miracles offers hope and spiritual awakening in the middle of
                 despair.</p>
+        </div>        
+        </div>
+
+        <div className="corky-image"><div className="overlay"><p className="overlayText">"My awareness was at a pinpoint. There was no thinking, no experiencing; there was no nothing. There was me in the light as part of the light and no thought."</p></div></div>
+        <div className="infoBox">
+         
+      
+            <h3>“Never have I had the pleasure of reading a memoir so thoughtful in its understanding of humanity, the preservation of memory, and a feeling of optimism that compels you to leave its pages rejuvenated and optimistic about what there is to come.” </h3>
+                <h4>– Sarah Tierney, copy editor</h4>
+                <br />
+<h3>"<span className="italic">Prodigious Son</span> is an honest and insightful look into a life well-lived and filled with colorful tales.”</h3>
+
+<h4>– Jack Bornoff, screenwriter</h4>
+<br />
+<h3>“<span className="italic">Prodigious Son</span> reflects hope in the face of catastrophe. Corky Matson is a remarkable person, soul and example of a life well lived.”</h3>
+<h4>– Russell Jaffe, MD, Ph.D. CCN</h4>
+<br />
+
+
         </div>
         </div>
     )

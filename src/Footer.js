75b287b9fbea4e499ee41aa2ba907bc90026f8d0
jokessenich <johnny.kessenich@gmail.com>
2020-07-04 21:09:44 -0700

    Removed old press release. Added footer with FB and amazon

--- src/Footer.js
+++ src/Footer.js
@@ -6,10 +6,10 @@ export default class Main extends React.Component{
 render(){
     return(
         <div className = "footer">
-                    <h4>Ralph "Corky" Matson</h4>
 
-        <h3>Prodigious Son</h3>
-        <h4>A Memoir of Miracles</h4>
+        <h3>Available August 22nd!! Preorder now from <a className = "bold" href = "https://www.amazon.com/Prodigious-Son-Ralph-Corky-Matson/dp/1098308875/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1593921951&sr=8-1">Amazon</a>. Follow us on Facebook!</h3>
+        <a className = "facebook-icon" href = "https://www.facebook.com/ProdigiousSonMemoir" target = "_blank"><img src = "http://clipart-library.com/images_k/facebook-icon-transparent-background/facebook-icon-transparent-background-20.png" height = "50" width = "50"></img>
+        </a>
         </div>
     )
 }

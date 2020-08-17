28f5275cbee4f25d747adfe6ecf969e58b7bf9d3
jokessenich <johnny.kessenich@gmail.com>
2020-08-16 21:06:39 -0700

    blog and front page change

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Blog.css'
 
 export default class Blog extends React.Component{
 
@@ -7,7 +8,19 @@ render(){
     return(
         <div>
         <h1> Blog </h1>
-        <h2>Coming Soon...</h2>
+        <div className = "postContainer">
+
+        <h2 className = "blogTitle">Official Release Date Approaching</h2>
+        <h3>August 16th 2020</h3>
+        <p className = "blogPost">On August 22nd we will be celebrating the official release of Prodigious Son: A Memoir of Miracles by Ralph "Corky" Matson and transcribed by John Kessenich!! <br /> <br />
+
+        Please click <a href ="https://tinyurl.com/prodigioussonbookamazon">HERE</a> to purchase on Amazon!<br /><br /> <br />
+
+        <a href = "https://www.youtube.com/watch?v=iAVSoPTqpVU&t=73s">Click here</a>  for an interview with Ralph "Corky" Matson on YouTube.<br /> <br />
+
+        We will stream a live interview on the Facebook Page at 1 PM PDT, 4 PM EST. <a href = "https://www.facebook.com/events/657893901747327/">Click here</a> to watch!
+        </p>
+        </div>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,8 @@ render(){
     return(
         <div className = "page">
                 {this.state.showPopup?<div className="available-popup">
-        <h3>Available in paperback, eBook or Kindle August 22nd! {<br/>}</h3>
+                    <h3><a href = "https://www.facebook.com/events/657893901747327/">LIVE Book Release w/ author Ralph "Corky" Matson on Facebook
+Saturday, August 22 at 1 PM (PDT)</a></h3>
         
     </div>:""}
                     <h1 className = "title" >Prodigious Son: A Memoir of Miracles</h1>

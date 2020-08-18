14f6979ca23ec5e7260aa46158d585781902d23a
jokessenich <johnny.kessenich@gmail.com>
2020-08-18 15:57:24 -0700

    press releases added

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -8,6 +8,21 @@ render(){
     return(
         <div>
         <h1> Blog </h1>
+
+        <div className = "postContainer">
+
+        <h2 className = "blogTitle">Press Release! Hot Off the Press!</h2>
+        <h3>August 18th 2020</h3>
+        <p className = "blogPost">Our first official press release is out and it looks amazing!! Under a week now until the official release date!<br /><br /><br />
+         <a href ="https://www.send2press.com/wire/inspirational-new-book-prodigious-son-a-memoir-of-miracles-reveals-remarkable-life-of-an-urban-shaman-ralph-matson/">Click here </a> to take a look at the press release and an interview with Corky!<br /> <br />
+
+         
+        Or <a href ="https://tinyurl.com/prodigioussonbookamazon">click here</a> to purchase on Amazon!<br /><br /> <br />
+
+        Let the good times roll.
+        </p>
+        </div>
+
         <div className = "postContainer">
 
         <h2 className = "blogTitle">Official Release Date Approaching</h2>
--- src/components/Media.js
+++ src/components/Media.js
@@ -25,8 +25,8 @@ render(){
         <ul className = "mediaList">
         <li><a href= "https://github.com/jokessenich/corky/raw/master/Press%20One%20Page_Prodigious%20Son-2.1.pdf" download = "Press_One_Page_ProdigiousSon">Media One Page</a></li>
         <li><a href= "https://github.com/jokessenich/corky/raw/master/BIO%20Press.pdf" download = "RalphMatsonBio">Author Bio</a></li>
-     {//   <li><a href = "https://github.com/jokessenich/corky/raw/master/PRESS%20RELEASE_Prodigious%20Son%20.pdf" download = "PressRelease_ProdigiousSon">Press Release</a></li>
-}
+        <li><a href = "https://www.send2press.com/wire/inspirational-new-book-prodigious-son-a-memoir-of-miracles-reveals-remarkable-life-of-an-urban-shaman-ralph-matson/">Press Release</a></li>
+
         <li><a className = "MediaPhotosButton" onClick = {()=>{this.setState({showPhoto: true})}}>Photos</a>
         {this.state.showPhoto? <ul className = "photosList"><li>Click to Download:</li><li><a href = {corky} download = "Ralph_Matson.jpg"><img src= {corky} width="150" height = "125" /> </a></li>
         <li><a href = {youngCorky} download="Young_Ralph_Matson"><img src= {youngCorky} width="150" height = "200" /></a></li>
